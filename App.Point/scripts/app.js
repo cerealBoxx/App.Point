32b1a1ebf5c22c7fa42868d6a885cc9ce29f61d2
Tsuki Bra <cookie.bra@gmail.com>
2014-09-28 12:29:25 -0500

    Changes made by Tsuki Bra (cookie.bra@gmail.com).

--- App.Point/scripts/app.js
+++ App.Point/scripts/app.js
@@ -1,10 +1,9 @@
-var ds = ds || {};
-var appMain = appMain || {};
-(function (scope) {
+var app = (function(){
+
     document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide();
-        appMain.everlive = new Everlive("cZswy0ZulYmXBaML");
-        console.log(appMain.everlive)
+        everlive = new Everlive("cZswy0ZulYmXBaML");
+        
         ds = new kendo.data.DataSource({
             data: [
                 {
@@ -18,12 +17,68 @@ var appMain = appMain || {};
                 },
         ]
         });
+                
+        var onBackKeyDown = function(e) {
+        e.preventDefault();
+
+        navigator.notification.confirm('Do you really want to exit?', function (confirmed) {
+            var exit = function () {
+                navigator.app.exitApp();
+            };
+
+            if (confirmed === true || confirmed === 1) {
+                // Stop EQATEC analytics monitor on app exit
+                if (analytics.isAnalytics()) {
+                    analytics.Stop();
+                }
+                AppHelper.logout().then(exit, exit);
+            }
+        }, 'Exit', ['OK', 'Cancel']);
+    };
+
+        var onDeviceReady = function () {
+            // Handle "backbutton" event
+            document.addEventListener('backbutton', onBackKeyDown, false);
+
+            navigator.splashscreen.hide();
+
+            if (analytics.isAnalytics()) {
+                analytics.Start();
+            }
+
+            // Initialize AppFeedback
+            if (app.isKeySet(appSettings.feedback.apiKey)) {
+                try {
+                    feedback.initialize(appSettings.feedback.apiKey);
+                } catch (err) {
+                    console.log('Something went wrong:');
+                    console.log(err);
+                }
+            } else {
+                console.log('Telerik AppFeedback API key is not set. You cannot use feedback service.');
+            }
+        };
+        
+        document.addEventListener('deviceready', onDeviceReady, false);
 
         new kendo.mobile.Application(document.body, {
             transition: 'slide'
         });
 
+        var AppHelper = {
 
+            resolvePictureUrl: function (id) {
+                if (id && id !== emptyGuid) {
+                    return el.Files.getDownloadUrl(id);
+                } else {
+                    return '';
+                }
+            },
 
+            // Date formatter. Return date in d.m.yyyy format
+            formatDate: function (dateString) {
+                return kendo.toString(new Date(dateString), 'MMM d, yyyy');
+            },
+        };
     }, false);
 }());
