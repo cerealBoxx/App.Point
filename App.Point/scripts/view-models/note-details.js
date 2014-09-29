fd9185996c221b0caece708190803784c3888b6b
Tsuki Bra <cookie.bra@gmail.com>
2014-09-29 12:30:33 -0500

    Changes made by Tsuki Bra (cookie.bra@gmail.com).

--- App.Point/scripts/view-models/note-details.js
+++ App.Point/scripts/view-models/note-details.js
@@ -4,7 +4,15 @@ var kendoMobileApp = kendoMobileApp || {};
 
 (function (scope) {
     scope.show = function(){
-        console.log('wat');
+        $("#note-details-container").append(note.title);
+        $("#note-details-container").append(note.content);
+        $("#note-details-container").append(note.alarm);
+        var img = $('<img>');
+                img.attr('src', note.imageData);
+               
+        $("#note-details-container").append(img);
+        
+        
     };
     scope.noteDetails = kendo.observable({
         init: function () {
